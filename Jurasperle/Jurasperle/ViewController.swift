d0f8dbefd91e120d12c0052a668e1210958b3522
Unknown <asdfgh5889@gmail.com>
2018-06-30 19:34:31 +0500

    Init

---
+++ Jurasperle/Jurasperle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Jurasperle
+//
+//  Created by Sherzod on 6/30/18.
+//  Copyright © 2018 Humanz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
