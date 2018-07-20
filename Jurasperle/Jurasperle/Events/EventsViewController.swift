646978a68c9adeff8e3d86055986f0dfc0595c01
dragon <sardor670@mail.ru>
2018-07-21 04:02:38 +0500

    fix bla bla

--- Jurasperle/Jurasperle/Events/EventsViewController.swift
+++ Jurasperle/Jurasperle/Events/EventsViewController.swift
@@ -10,12 +10,23 @@ import UIKit
 
 class EventsViewController: UIViewController {
 
+    @IBOutlet weak var eventBody: UIWebView!
+    @IBOutlet weak var eventTitle: UILabel!
+    @IBOutlet weak var eventDate: UILabel!
+    @IBOutlet weak var eventImage: UIImageView!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        setUpEvents()
+       eventBody.loadHTMLString("hjfdbvhjdsvbhdsbvjhdbvkjhfvblhdbjkdfjhdfvbkjdbvjhdbjhdbdhbfdfsjhdfbjhdfbjfdkvdfjvhfbdvjsdfbkjsdfkjsdvbkjdsvbdfjkvbdfjs", baseURL: nil)
+        eventDate.text = "61546565163"
+        eventTitle.text = "jsdvhgdvkjdhl kasvkh k adsvkguka kausdg kuadgfkuasdgfasudf uadsf gagsodiusadgaiusy"
     }
 
-  
+  func setUpEvents()
+  {
+    
+    
+   }
 
 }
